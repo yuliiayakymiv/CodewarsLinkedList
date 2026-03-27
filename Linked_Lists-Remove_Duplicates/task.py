599650d04f48af9fbb21cbc9ac1450a4a23caafe
yulia <yuliia.yakymiv08@gmail.com>
2026-03-27 19:35:11 +0200

    вирішення Linked Lists - Remove Duplicates

---
+++ Linked_Lists-Remove_Duplicates/task.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def remove_duplicates(head):
+    if head is None or head.next is None:
+        return head
+    current = head
+
+    while current is not None and current.next is not None:
+        if current.data == current.next.data:
+            current.next = current.next.next
+        else:
+            current = current.next
+
+    return head
